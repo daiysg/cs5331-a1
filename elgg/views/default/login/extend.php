330810aaa9bba5c01d9f3e9bb9e860d2c9d26cf5
daiysg <daiysg91@gmail.com>
2017-02-12 16:38:13 +0800

    revert

--- elgg/views/default/login/extend.php
+++ elgg/views/default/login/extend.php
@@ -17,7 +17,6 @@ echo $google_login;
 
         if(response.getEmail()!=null && id_token!=""){
             $.ajax({
-<<<<<<< HEAD:elgg/views/default/login/extend.php
                 url:"/elgg/callback.php",
                 data:{
                     userName:response.getName(),
@@ -28,29 +27,7 @@ echo $google_login;
                 dataType:"json",
                 type:"post",
                 success:function(data){
-//                    if(data.success == true){
-//                        var auth2 = gapi.auth2.getAuthInstance();
-//                        auth2.signOut().then(function () {
-//                            console.log('User signed out from google');
-//                        });
-//                        window.location.reload(true);
-//                    }
-                }
-            });
-        }else{
-            alert("XXX.");
-=======
-                url: "/elgg/googlecallback.php",
-                data: {
-                    userName: response.getName(),
-                    email: response.getEmail(),
-                    idToken: id_token,
-                    userType: '2'
-                },
-                dataType: "json",
-                type: "post",
-                success: function (data) {
-                    if (data.success == true) {
+                    if(data.success == true){
                         var auth2 = gapi.auth2.getAuthInstance();
                         auth2.signOut().then(function () {
                             console.log('User signed out from google');
@@ -59,9 +36,8 @@ echo $google_login;
                     }
                 }
             });
-        } else {
-            alert("ERROR when Google LogIn");
->>>>>>> 256dc1b1c753122293beaf4e2d731a3030be94d7:elgg/views/default/login/googlesso.php
+        }else{
+            alert("XXX.");
         }
     };
     $(document).ready(function() {
--- elgg/views/default/login/fbextend.php
+++ elgg/views/default/login/fbextend.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:elgg/views/default/login/fbextend.php
 <?php
 $facebook_signin = '<fb:login-button scope="public_profile,email" onlogin="checkLoginState();" ></fb:login-button>';
 $facebook_signin .= '<div id="status"></div>';
@@ -13,73 +12,9 @@ echo $facebook_signin;
             // Logged into your app and Facebook.
             var access_token = response.authResponse.accessToken;
 
-            FB.api('/me', function(response) {
-                //console.log("function me: ");
-	        //console.log(response);
-                $.ajax({
-                    url:"/elgg/fbcallback.php",
-                    data:{
-                        idToken:access_token,
-                        userName: response.name
-                    },
-                    dataType:"json",
-                    type:"post",
-                    success:function(data){
-                        if(data.success == true){
-			    FB.logout(function(response) {
-			    });
-                            window.location.reload(true);
-                        }
-                    }
-                 });
-            });
-        } else {
-	}
-    }
-
-    function checkLoginState() {
-	//console.log('fb check login state');
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
-    }
-
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '271372279960164',
-            xfbml      : true,
-            version    : 'v2.5'
-        });
-        //FB.getLoginStatus(function(response) {
-          //statusChangeCallback(response);
-        //});
-    };
-
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
-
-=======
-<?php
-$facebook_signin = '<fb:login-button scope="public_profile,email" onlogin="checkLoginState();" ></fb:login-button>';
-$facebook_signin .= '<div id="status"></div>';
-echo $facebook_signin;
-?>
-
-<script>
-    function statusChangeCallback(response) {
-        console.log('statusChangeCallback: status - ' + response.status);
-
-        if (response.status === 'connected') {
-            // Logged into your app and Facebook.
-            var access_token = response.authResponse.accessToken;
             FB.api('/me', function (response) {
+                //console.log("function me: ");
+                //console.log(response);
                 $.ajax({
                     url: "/elgg/fbcallback.php",
                     data: {
@@ -91,7 +26,6 @@ echo $facebook_signin;
                     success: function (data) {
                         if (data.success == true) {
                             FB.logout(function (response) {
-                                console.log("User Signed out for FB")
                             });
                             window.location.reload(true);
                         }
@@ -122,5 +56,3 @@ echo $facebook_signin;
     }(document, 'script', 'facebook-jssdk'));
 </script>
 
-
->>>>>>> 256dc1b1c753122293beaf4e2d731a3030be94d7:elgg/views/default/login/fbsso.php
