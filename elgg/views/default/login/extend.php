9f87e6e0e76291b3fbf3c57d6874d7b731882545
daiysg <daiysg91@gmail.com>
2017-02-12 15:16:06 +0800

    update google client id

--- elgg/views/default/login/extend.php
+++ elgg/views/default/login/extend.php
@@ -1,6 +1,5 @@
 <?php
 $google_login = '<div class="g-signin2" data-onsuccess="onSignIn" ></div>';
-
 echo $google_login;
 ?>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
@@ -8,11 +7,14 @@ echo $google_login;
     function onSignIn(googleUser) {
         // Useful data for your client-side scripts:
         var profile = googleUser.getBasicProfile();
+        console.log('Name: ' + profile.getName());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
         var response = profile;
 
         // The ID token you need to pass to your backend:
         var id_token = googleUser.getAuthResponse().id_token;
-        //console.log("ID Token: " + id_token);
+        console.log("ID Token: " + id_token);
 
         if(response.getEmail()!=null && id_token!=""){
             $.ajax({
@@ -29,7 +31,7 @@ echo $google_login;
                     if(data.success == true){
                         var auth2 = gapi.auth2.getAuthInstance();
                         auth2.signOut().then(function () {
-                            //console.log('User signed out from google');
+                            console.log('User signed out from google');
                         });
                         window.location.reload(true);
                     }
@@ -41,7 +43,7 @@ echo $google_login;
     };
     $(document).ready(function() {
         $("head").append('<meta name="google-signin-scope" content="profile email">');
-        $("head").append('<meta name="google-signin-client_id" content="539284137631-cjfnmmabld2lok5s79jjt1pj7psucq4q.apps.googleusercontent.com">');
+        $("head").append('<meta name="google-signin-client_id" content="423648320802-i04pigmqtclahsf9m672l2cblafh0o9h.apps.googleusercontent.com">');
     });
 </script>
 
