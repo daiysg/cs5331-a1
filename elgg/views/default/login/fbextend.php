6452bb48aba5544c657141754f3268804eed2c1b
daiysg <daiysg91@gmail.com>
2017-02-12 16:31:48 +0800

    Revert "format and change the refresh method"
    This reverts commit 41dee77d6fa2e0b92fc046fd0dd51ce015c07b2f.

--- elgg/views/default/login/fbextend.php
+++ elgg/views/default/login/fbextend.php
@@ -6,58 +6,58 @@ echo $facebook_signin;
 
 <script>
     function statusChangeCallback(response) {
-        console.log('statusChangeCallback: status - ' + response.status);
+        //console.log('statusChangeCallback: status - ' + response.status);
 
         if (response.status === 'connected') {
             // Logged into your app and Facebook.
             var access_token = response.authResponse.accessToken;
-            FB.api('/me', function (response) {
+
+            FB.api('/me', function(response) {
+                //console.log("function me: ");
+	        //console.log(response);
                 $.ajax({
-                    url: "/elgg/fbcallback.php",
-                    data: {
-                        idToken: access_token,
+                    url:"/elgg/fbcallback.php",
+                    data:{
+                        idToken:access_token,
                         userName: response.name
                     },
-                    dataType: "json",
-                    type: "post",
-                    success: function (data) {
-                        if (data.success == true) {
-                            FB.logout(function (response) {
-                            });
-                            window.location.reload();
+                    dataType:"json",
+                    type:"post",
+                    success:function(data){
+                        if(data.success == true){
+			    FB.logout(function(response) {
+			    });
+                            window.location.reload(true);
                         }
                     }
-                });
+                 });
             });
         } else {
-        }
+	}
     }
 
     function checkLoginState() {
-        //console.log('fb check login state');
-        FB.getLoginStatus(function (response) {
+	//console.log('fb check login state');
+        FB.getLoginStatus(function(response) {
             statusChangeCallback(response);
         });
     }
 
-    window.fbAsyncInit = function () {
+    window.fbAsyncInit = function() {
         FB.init({
-            appId: '271372279960164',
-            xfbml: true,
-            version: 'v2.5'
+            appId      : '271372279960164',
+            xfbml      : true,
+            version    : 'v2.5'
         });
         //FB.getLoginStatus(function(response) {
-        //statusChangeCallback(response);
+          //statusChangeCallback(response);
         //});
     };
 
-    (function (d, s, id) {
+    (function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
